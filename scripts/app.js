515b131dd6ca05675b5d37807fa15292ba6b87f4
MarcKunst <marckunst93@gmail.com>
2019-11-13 16:37:00 +0100

    d3 working

--- scripts/app.js
+++ scripts/app.js
@@ -31,9 +31,10 @@ runQuery(endpoint, queryWeaponsJapan)
     function handleData(data) {
         const cleanedData = loopData(data);
         const nestedData = nestObjects(cleanedData);
-        console.log(nestedData);
         // schrijf nieuwe const voor aangepaste data
+
         nestObjects(cleanedData)
+        d3Circles(nestedData)
         // geef hier de data weer door aan de volgende functie
     }  
 
@@ -56,7 +57,7 @@ runQuery(endpoint, queryWeaponsJapan)
         const expensesByType = d3.nest()
         .key(function(d) { return d.type; })
         .rollup(function(v) { return v.length; })
-        .entries(cleanedData);
+        .entries(cleanedData)
         return expensesByType;
     }
 
@@ -71,4 +72,78 @@ function runQuery(url, query) {
 }
 
 //d3
+
+function d3Circles(nestedData){
+    const dataset = {
+        children: nestedData
+    };
+    console.log(dataset)
+    let diameter = 600;
+        let color = d3.scaleOrdinal(d3.schemeCategory20);
+
+        let bubble = d3.pack(dataset)
+            .size([diameter, diameter])
+            .padding(1.5);
+
+        let svg = d3.select("body")
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+
+        let nodes = d3.hierarchy(dataset)
+            .sum(function(d) { return d.value; });
+
+        let node = svg.selectAll(".node")
+            .data(bubble(nodes).descendants())
+            .enter()
+            .filter(function(d){
+                return  !d.children
+            })
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        node.append("title")
+            .text(function(d) {
+                return d.key + ": " + d.value;
+            });
+
+        node.append("circle")
+            .attr("r", function(d) {
+                return d.r;
+            })
+            .style("fill", function(d,i) {
+                return color(i);
+            });
+
+        node.append("text")
+            .attr("dy", ".2em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.key.substring(0, d.r / 3);
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+
+        node.append("text")
+            .attr("dy", "1.3em")
+            .style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data.value;
+            })
+            .attr("font-family",  "Gill Sans", "Gill Sans MT")
+            .attr("font-size", function(d){
+                return d.r/5;
+            })
+            .attr("fill", "white");
+
+        d3.select(self.frameElement)
+            .style("height", diameter + "px");
+}        
 
